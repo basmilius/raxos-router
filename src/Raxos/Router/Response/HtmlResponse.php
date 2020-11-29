1af020c0552053c3c59fe18da6e039f1376a84ba
Bas Milius <basmilius@hotmail.com>
2020-11-29 13:35:18 +0100

    feat(response): Adds html response.

---
+++ src/Raxos/Router/Response/HtmlResponse.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Raxos\Router\Response;
+
+/**
+ * Class HtmlResponse
+ *
+ * @author Bas Milius <bas@glybe.nl>
+ * @package Raxos\Router\Response
+ * @since 2.0.0
+ */
+class HtmlResponse extends Response
+{
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	protected function respondBody(): void
+	{
+		echo (string)$this->value;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	protected function respondHeaders(): void
+	{
+		if (!array_key_exists('Content-Type', $this->headers))
+		{
+			$this->headers['Content-Type'] = 'text/html';
+		}
+
+		parent::respondHeaders();
+	}
+
+}
--- src/Raxos/Router/Response/ResponseMethods.php
+++ src/Raxos/Router/Response/ResponseMethods.php
@@ -20,62 +20,83 @@ use function is_array;
 trait ResponseMethods
 {
 
-    private static array $headers = [];
+	private static array $headers = [];
 
-    /**
-     * Adds the given header to the response.
-     *
-     * @param string $name
-     * @param string $value
-     * @param bool $replace
-     *
-     * @return $this
-     * @author Bas Milius <bas@mili.us>
-     * @since 1.0.0
-     */
-    protected function header(string $name, string $value, bool $replace = true): static
-    {
-        if (array_key_exists($name, static::$headers) && !$replace) {
-            if (is_array(static::$headers[$name])) {
-                static::$headers[$name][] = $value;
-            } else {
-                static::$headers[$name] = [static::$headers[$name], $value];
-            }
-        } else {
-            static::$headers[$name] = $value;
-        }
+	/**
+	 * Adds the given header to the response.
+	 *
+	 * @param string $name
+	 * @param string $value
+	 * @param bool $replace
+	 *
+	 * @return $this
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	protected function header(string $name, string $value, bool $replace = true): static
+	{
+		if (array_key_exists($name, static::$headers) && !$replace)
+		{
+			if (is_array(static::$headers[$name]))
+			{
+				static::$headers[$name][] = $value;
+			}
+			else
+			{
+				static::$headers[$name] = [static::$headers[$name], $value];
+			}
+		}
+		else
+		{
+			static::$headers[$name] = $value;
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Returns a JSON response.
-     *
-     * @param mixed $value
-     *
-     * @return JsonResponse
-     * @author Bas Milius <bas@mili.us>
-     * @since 1.0.0
-     */
-    #[Pure]
-    protected final function json(mixed $value): JsonResponse
-    {
-        return new JsonResponse($this->router, [], $value);
-    }
+	/**
+	 * Returns a HTML response.
+	 *
+	 * @param mixed $value
+	 *
+	 * @return HtmlResponse
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	#[Pure]
+	protected final function html(string $value): HtmlResponse
+	{
+		return new HtmlResponse($this->router, [], $value);
+	}
 
-    /**
-     * Returns a redirect effect.
-     *
-     * @param string $destination
-     * @param int $responseCode
-     *
-     * @return RedirectEffect
-     * @author Bas Milius <bas@mili.us>
-     * @since 1.0.0
-     */
-    protected final function redirect(string $destination, #[ExpectedValues(flagsFromClass: HttpCode::class)] int $responseCode = HttpCode::TEMPORARY_REDIRECT): RedirectEffect
-    {
-        return new RedirectEffect($this->router, $destination, $responseCode);
-    }
+	/**
+	 * Returns a JSON response.
+	 *
+	 * @param mixed $value
+	 *
+	 * @return JsonResponse
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	#[Pure]
+	protected final function json(mixed $value): JsonResponse
+	{
+		return new JsonResponse($this->router, [], $value);
+	}
+
+	/**
+	 * Returns a redirect effect.
+	 *
+	 * @param string $destination
+	 * @param int $responseCode
+	 *
+	 * @return RedirectEffect
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	protected final function redirect(string $destination, #[ExpectedValues(flagsFromClass: HttpCode::class)] int $responseCode = HttpCode::TEMPORARY_REDIRECT): RedirectEffect
+	{
+		return new RedirectEffect($this->router, $destination, $responseCode);
+	}
 
 }
